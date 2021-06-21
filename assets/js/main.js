ef7298f205d23a7dbcb80f66794bbed37765f6df
dmobrien <dmobrienllc@gmail.com>
2021-06-21 17:55:36 -0500

    Message persistence completed, shaded rows yet to do

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,97 +1,124 @@
 $(function() {
-    ///let timeDiv = $('#timer');
-    // timerDiv.textContent = moment().format("MMM Do YY");
 
-    // let today = moment();
-    // // $("#9a").text(today.format("MMM Do, YYYY"));
-    // // today('12:16','HH:mm').minutes();
+    function appendToStorage(key, data){
+       
+        let currText = localStorage.getItem(key);
 
-    // let hoursMins = today.set({"hour": 9, "minute": 00});
-    // $("#9a").text(hoursMins.format("HH:mm"));
-
-    // console.log(hoursMins);
-
-    // let am10 = today.set({"hour": 10, "minute": 00});
-    // $("#10a").text(am10.format("HH:mm"));
-
-    // let am11 = today.set({"hour": 11, "minute": 00});
-    // $("#11a").text(am11.format("HH:mm"));
-    function appendToStorage(name, data){
-      var old = localStorage.getItem(name);
-      if(old === null) {
-        old = "";
+        //instead of this maybe blow it away and just enter whatever comes
+        //in? 
+        if(currText === null) {
+          currText = "";
+          localStorage.setItem(key,currText);
+        }
+        else if(currText===data){
+          console.log("Data === currText, not appending.");
+        }
+        else{
+          console.log("appended: " + data + "To currData: " + currText) ;
+          currText += data;
+          localStorage.setItem(key,data);
+        }
       }
-      localStorage.setItem(name, old + data);
-    }
-
+    
     function init(){
       buildScheduler();
-
-      refreshValues();
-    }
-    
-    //loop through all my textarea divs and refresh data
-    function refreshValues(){
-      let textarea = $(".schedule-item").filter("[data-text='9']");
-      textarea.text(localStorage.getItem("data-text-index:9"));
     }
 
-    function buildScheduler()
-    {
+    function buildScheduler(){
         let containerDiv = $("<div>").addClass("container my-container");
-        // Delegate event listener to the parent element, <div id="buttons">
+        //BEGIN BUILD CALLBACK
         containerDiv.on('click', '.save-button', function (event) {
 
-          //displayLetterEl.addClass('letter');
-          if ($(this).attr("id") == "something") {
-            //var html  = $(this).html(); //here you can use any of the above method as per requirement.
-         }
          let index = $(this).attr("data-time");
          let text = $(".schedule-item").filter("[data-text='" + index + "']").val();
 
-         alert($(event.target).attr('data-time'));
          alert("Textarea value: " + text)
 
-         //if local storage for this item doesn't exist, create,
-         //otherwise append it. If the text field comes back empty
-         //assume user wants to clear all. Delete the iterm.
          appendToStorage(("data-text-index:" + index),text);
-
-         alert("From local storage: " + localStorage.getItem("data-text-index:" + index));
-          
-        //  $('.address').filter(':not([data-address-type]), [data-address-type="to"]').html('')
  
-        });//container event listener add
+        });
+        //END BUILD CALLBACK
 
         $("body").append(containerDiv);
-        let today = moment();
-        console.log("Parent Row: " + containerDiv);
-        let hoursMins;
 
-        for (var i = 9; i < 18; i++) {
+        // let timeAdd1 = seedTime.add(1,'hours');
+        // console.log("Seed + 1 : " + hoursTmp.format("HH:mm"));
+
+        // if(today.isAfter(seedTime)){
+        //   alert("We are after.")
+        // }
+        // else if(today.isBefore(seedTime)){
+        //   alert("We are before");
+        // }
+        // else{
+        //   alert("We are between")
+        // }
+
+        //our seed date today at start of business hours
+        let today = moment();
+        let seedTime = moment({ y: today.year(), M: today.month(), d: today.day(), h: 11, m: 0, s: 0, ms: 0}); 
+        let tmpTime;
+        let oldTime;
+
+        for (var i = 0; i < 10; i++) {
 
             let rowDiv = $("<div>").addClass("row my-row pl-0");
 
-            //TODO: Dyanmically add time
             let timeDiv = $("<div>").addClass("col-1 my-col my-col-left time-block");
             let pTimeEl = $("<p>").attr("id",(i));
-            
-            hoursMins = today.set({"hour": i, "minute": 00});
-            pTimeEl.text(hoursMins.format("HH:mm"));
+
+            if(i===0){
+              tmpTime = seedTime;
+              oldTime = tmpTime;
+            }            
+            else{
+              oldTime = tmpTime;
+              tmpTime = tmpTime.add(1,'hours');
+            }
+
+            pTimeEl.text(tmpTime.format("HH:mm"));
             timeDiv.append(pTimeEl);
             rowDiv.append(timeDiv);
 
             let msgDiv = $("<div>").addClass("col-9 my-col");
-            if(hoursMins.isBefore(today))
-            {
-              msgDiv.addClass("past");
-            }else if(hoursMins.isAfter(today)){
-              msgDiv.addClass("future");
-            }else{
+
+            console.log("Today hours: " + today.format("HH:mm"));
+            console.log("Tmp hours: " + tmpTime.format("HH:mm"));
+            console.log("old hours: " + oldTime.format("HH:mm"));
+
+            if(today.isBetween(oldTime,tmpTime,'hours'))
+            { 
+              console.log("present");
               msgDiv.addClass("present");
             }
+
+            if(tmpTime.isBefore(today,'hours'))
+            {
+              console.log("past");
+              msgDiv.addClass("past");
+            }
+
+            if(tmpTime.isAfter(today,'hours')){
+              console.log("future");
+              msgDiv.addClass("future");
+            }
+
+            console.log("Before/After: " + today.isAfter(tmpTime, 'hours')); 
+
+            //If local storage exists for this item, also append that text
             let txtAreaEl = $("<textarea>").attr("placeholder","Text Here").attr("data-text",i);
             txtAreaEl.addClass("schedule-item");
+            
+            //TODO: Make sure these aren't duplicated
+            let storageKey = "data-text-index:" + i;
+            if(localStorage.getItem(storageKey)===null){
+              localStorage.setItem(storageKey,"");
+              txtAreaEl.text("");
+            }
+            else{
+              txtAreaEl.text(localStorage.getItem(storageKey));
+            }
+
             msgDiv.append(txtAreaEl);
             rowDiv.append(msgDiv);
 
@@ -110,22 +137,7 @@ $(function() {
     init();
   });
 
-  //             <script>
-  // $( ".myClass" ).css( "border", "3px solid red" );
-  // </script>
-            
-            // let hoursMins = today.set({"hour": 9, "minute": 00});
-            // $("#9a").text(hoursMins.format("HH:mm"));
-  
 
-            // <div class="row my-row pl-0">
-            //   <div class="col-1 my-col my-col-left time-block">
-            //     <p id="9a"></p>
-            //   </div>
-            // <div class="col-9 my-col past">
-            //   <textarea placeholder="Text here" aria-placeholder="Your text here"></textarea>
-            // </div>
-            // <div class="col-2 my-col">
-            //   <button type="button" class="saveBtn">Sv img</button>
-            // </div>
-            // </div>    
+
+
+ 
